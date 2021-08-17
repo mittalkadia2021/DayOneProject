c7be416d54106060808be07faedba655ebdd84f2
Mittal <mittalkadia2021@gmail.com>
2021-08-17 21:44:48 +0100

    Hi

--- src/test/java/org/example/TestSuite.java
+++ src/test/java/org/example/TestSuite.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class TestSuite extends BaseTest{
     @Test
     public void user_Able_To_Open_ASDA_Website(){
+
         System.out.println("ASDA website is open");
     }
 }
